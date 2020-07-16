f42a427c964695f496ce5ecc94a041662ea0e2cc
phanumax@gmail.com <phanumax@gmail.com>
2020-07-16 11:00:09 +0700

    20200716

--- sync-to-backup.cmd
+++
@@ -1,5 +0,0 @@
-Echo "Starting sync to backup server"
-
-start /wait /B "sync_to_backup" robocopy "C:\ProgramData\KasperskySC\SC_Backup" "\\hqsrvwinpbk84\nonsap\SC_Backup" /r:60 /w:5 /PURGE /MIR /MT:64 /LOG+:c:\tmp\sc_backup.txt
-
-Echo "Done"
